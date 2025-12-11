ab24afb868ee38ef34e3ac51a8cd66f9440be9b9
JonaDevnet <jonathan.rinaldi03@gmail.com>
2025-12-11 11:06:47 -0300

    Creacion de metodo asincrono

---
+++ BackEnd/Controllers/SomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet("sync")]
+        public IActionResult GetSync()
+        {
+
+            //Cronometro
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Conexion a BD terminada");
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Envio de mail teminado");
+
+            Console.WriteLine("Todo finalizado");
+            stopwatch.Stop();
+
+            //Medimos el tiempo en milisegundos
+            long tiempoEnMs = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Tardo: {tiempoEnMs} ms");
+
+            //otra opcion en horas:minutos,segundos
+            TimeSpan tiempoTotal = stopwatch.Elapsed;
+            Console.WriteLine($"Tardo: {tiempoTotal.TotalSeconds} s");
+
+
+            return Ok();
+        }
+    }
+}
